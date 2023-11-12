b077c42607e587d2d04d1874a7d8377f73332984
Super <camilashams97@gmail.com>
2023-11-12 13:48:06 +0300

    add file

--- pages/new_page_contakts.py
+++ pages/new_page_contakts.py
@@ -1,11 +1,9 @@
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
-import time
+
 
 class Contacts2Page(BasePage):
     def check_region(self):
          pagetitle = self.browser.title
-         pageurl = self.browser.current_url
          assert "Камчатский край" in pagetitle, "Error"
          print("Проверка названия страницы успешно пройдена")
          get_url = self.browser.current_url
